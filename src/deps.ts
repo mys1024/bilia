822b56b53fd60ddc884b7e8c30882f135224a1fe
mys1024 <mys.1024@outlook.com>
2022-06-10 02:50:53 +0800

    chore: pin `esm.sh` modules' build version

--- src/deps.ts
+++ src/deps.ts
@@ -4,9 +4,9 @@ export { ensureDir, ensureFile } from "https://deno.land/std@0.142.0/fs/mod.ts";
 
 export { Program } from "https://deno.land/x/program@0.1.6/mod.ts";
 
-import _dayjs from "https://esm.sh/dayjs@1.11.3";
-import utc from "https://esm.sh/dayjs@1.11.3/plugin/utc.js";
-import timezone from "https://esm.sh/dayjs@1.11.3/plugin/timezone.js";
+import _dayjs from "https://esm.sh/dayjs@1.11.3?pin=v86";
+import utc from "https://esm.sh/dayjs@1.11.3/plugin/utc.js?pin=v86";
+import timezone from "https://esm.sh/dayjs@1.11.3/plugin/timezone.js?pin=v86";
 _dayjs.extend(utc);
 _dayjs.extend(timezone);
 export const dayjs = _dayjs;
